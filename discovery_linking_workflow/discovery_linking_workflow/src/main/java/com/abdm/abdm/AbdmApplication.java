edee0785f3446647c159ebd7cd76a287402df886
Manika Jain <manikajain11@gmail.com>
2023-07-13 01:54:52 +0530

    Initial commit

---
+++ discovery_linking_workflow/discovery_linking_workflow/src/main/java/com/abdm/abdm/AbdmApplication.java
@@ -0,0 +1,13 @@
+package com.abdm.abdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbdmApplication.class, args);
+	}
+
+}
