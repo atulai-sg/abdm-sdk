6b72ee09c7b1ea54556cc5279e08132755bc8dc0
MDZAID123 <88980148+MDZAID123@users.noreply.github.com>
2023-07-13 15:24:32 +0530

    Initial gradle template

---
+++ ABDM_Consent_Management/src/test/java/com/zaid/consent/AbdmConsentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaid.consent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbdmConsentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
