ed721c17e2dc8798f5a606727ce2dfc4a29646b7
Suyash Dubey <suyash20251@iiitd.ac.in>
2023-07-13 11:54:47 +0530

    first commit

---
+++ Desktop/workspace-spring-tool-suite-4-4.19.0.RELEASE/SprinngBootRestfulApp/src/main/java/com/suyash/SprinngBootRestfulAppApplication.java
@@ -0,0 +1,13 @@
+package com.suyash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinngBootRestfulAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinngBootRestfulAppApplication.class, args);
+	}
+
+}
